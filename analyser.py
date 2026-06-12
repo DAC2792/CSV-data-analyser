fa0b12d7ff58c0e7673fc49334d0e2ccb5260710
David <davidamorycrawford@gmail.com>
2026-06-12 21:11:19 +0100

    Add error handling for invalid column names

--- analyser.py
+++ analyser.py
@@ -10,24 +10,33 @@ def load_csv(filepath):
     return df
 
 #Print key information and statistics about the dataframe
-def analyse_csv(df):
+def analyse_csv(df,column):
     print(f"Rows and Columns: {df.shape}")
     print(f"Columns: {df.columns}")
     print(f"Summary Statistics: {df.describe()}")
-    print(f"Max value: {df['sales'].max()}")
-    print(f"Minimum value: {df['sales'].min()}")
+    try:
+        print(f"Max value: {df[column].max()}")
+        print(f"Minimum value: {df[column].min()}")
+    except KeyError:
+        print(f"Error: '{column}' is not a valid column.")
+        return 
 
     with open("outputs/results.txt", "w") as f:
         f.write(f"Rows and Columns: {df.shape}\n")
         f.write(f"Columns: {df.columns}\n")
         f.write(f"Summary Statistics: {df.describe()}\n")
-        f.write(f"Max value: {df['sales'].max()}\n")
-        f.write(f"Minimum value: {df['sales'].min()}\n")
+        try:
+            f.write(f"Max value: {df[column].max()}\n")
+            f.write(f"Minimum value: {df[column].min()}\n")
+        except KeyError:
+            print(f"Error: '{column}' is not a valid column.")
+        return 
 
 #Call main block to begin running all code
 if __name__ == "__main__":
     file = input("Enter CSV filename: ")
     df = load_csv(file)
     if df is not None:
-        analyse_csv(df)
+        column = input("Enter column name to analyse: ")
+        analyse_csv(df,column)
 
