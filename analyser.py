59e0f50740d19516c7626b8165b8d08f4c20e11d
David <davidamorycrawford@gmail.com>
2026-06-13 16:05:52 +0100

    Support analysing multiple columns with input cleaning

--- analyser.py
+++ analyser.py
@@ -10,33 +10,39 @@ def load_csv(filepath):
     return df
 
 #Print key information and statistics about the dataframe
-def analyse_csv(df,column):
+def analyse_csv(df,columns):
     print(f"Rows and Columns: {df.shape}")
     print(f"Columns: {df.columns}")
     print(f"Summary Statistics: {df.describe()}")
-    try:
-        print(f"Max value: {df[column].max()}")
-        print(f"Minimum value: {df[column].min()}")
-    except KeyError:
-        print(f"Error: '{column}' is not a valid column.")
-        return 
+    for column in columns:
+        try:
+            print(f"Max value: {df[column].max()}")
+            print(f"Minimum value: {df[column].min()}")
+        except KeyError:
+            print(f"Error: '{column}' is not a valid column.")
 
     with open("outputs/results.txt", "w") as f:
         f.write(f"Rows and Columns: {df.shape}\n")
         f.write(f"Columns: {df.columns}\n")
         f.write(f"Summary Statistics: {df.describe()}\n")
-        try:
-            f.write(f"Max value: {df[column].max()}\n")
-            f.write(f"Minimum value: {df[column].min()}\n")
-        except KeyError:
-            print(f"Error: '{column}' is not a valid column.")
-        return 
+        for column in columns:
+            try:
+                f.write(f"Max value: {df[column].max()}\n")
+                f.write(f"Minimum value: {df[column].min()}\n")
+            except KeyError:
+                print(f"Error: '{column}' is not a valid column.")
 
 #Call main block to begin running all code
 if __name__ == "__main__":
     file = input("Enter CSV filename: ")
     df = load_csv(file)
     if df is not None:
-        column = input("Enter column name to analyse: ")
-        analyse_csv(df,column)
+        column = input("Enter column name(s), separated by commas: ")
+        raw_columns = column.split(",")
+        columns = []
 
+        for item in raw_columns:
+            cleaned = item.strip()
+            columns.append(cleaned)
+
+        analyse_csv(df,columns)
